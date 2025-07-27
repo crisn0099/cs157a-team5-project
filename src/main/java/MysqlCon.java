0f00d3fa182de17fbe1cf898a8bb1ed9e7dbb8a6
Cristian Navarro <crisn0099@gmail.com>
2025-07-26 18:07:00 -0700

    Update MysqlCon.java with main test method and connection method

--- src/main/java/MysqlCon.java
+++ src/main/java/MysqlCon.java
@@ -3,7 +3,6 @@ import java.sql.*;
 public class MysqlCon {
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // Use .cj for MySQL 8+
             Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/games_for_me?autoReconnect=true&useSSL=false",
                 "root",
@@ -11,17 +10,33 @@ public class MysqlCon {
             );
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Student");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM game");
 
             while (rs.next()) {
-                System.out.println(rs.getInt(1) + " " +
-                                   rs.getString(2) + " " +
-                                   rs.getString(3));
+                System.out.println(
+                    rs.getInt("gameID") + " " +
+                    rs.getString("title") + " " +
+                    rs.getString("releaseDate")
+                );
             }
 
             con.close();
         } catch (Exception e) {
             System.out.println("SQLException caught: " + e);
         }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/games_for_me?autoReconnect=true&useSSL=false",
+            "root",
+            "DBpassword"
+        );
     }
 }
